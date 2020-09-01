3dd022d8b87221b0b9182cd40ae4964a3bc82941
mBerdal <magberd@stud.ntnu.no>
2020-09-01 10:14:54 +0200

    added description to obstacle constructor

--- obstacle/obstacle.py
+++ obstacle/obstacle.py
@@ -1,5 +1,10 @@
 
 class Obstacle():
+
+  """
+  Takes as parameter a function 'fun' which takes as parameters coordinates (x, y),
+  and returns a boolean which indicates wether or not the obstacle is at (x, y)
+  """
   def __init__(self, fun) -> None:
     self.fun = fun
 
