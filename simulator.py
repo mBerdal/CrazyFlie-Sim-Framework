e28ee549d4c28fafd3e751fdaf565786f86d0227
erllon <lone.erlend@gmail.com>
2020-09-08 13:19:57 +0200

    Added simulation.py and logger.py. Nothing implemented

---
+++ simulator.py
@@ -0,0 +1,46 @@
+import logger
+
+class Simulator():
+    """
+  Class representing the simulator itself
+  ...
+
+  Attributes
+  ----------
+  map: map-class(?)
+    Map of the environment
+  drones: drone-class(?)
+    List of all drones that are part of the simulation
+  commands: string array
+    Commands that can be sent to the drone(s)
+  logger: logger
+  
+  Methods
+  ----------
+  sim_step()
+    Simulating one step
+
+  collision_check()
+    Cheking if two or more of the drones in the simulation will collide
+
+  send_information()
+   TODO: Send information where?
+
+  log()
+    Sends information to the logger to save the data
+  """
+
+    def __init__(self):
+        pass
+
+    def sim_step(self, update_one_step):
+        pass
+    
+    def collision_check(self):
+        pass
+
+    def send_information(self):
+        pass
+
+    def log(self):
+        def
